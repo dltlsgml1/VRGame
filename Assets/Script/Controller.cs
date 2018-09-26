78b6b5bdbeb296d2fbb1162a50e41a1c49845fe4
LEE@Desktop <wmfehdgml@gmail.com>
2018-09-27 03:36:20 +0900

    컨트롤러 ok

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+
+    public GameObject Camera;
+    public Text InputText;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+        Vector2 InputPoint = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+        Vector3 InputVec;
+        InputVec.x = InputPoint.x;
+        InputVec.y = 0.0f;
+        InputVec.z = InputPoint.y;
+        Vector3.Normalize(InputVec);
+        InputVec = InputVec * 0.1f;
+        Camera.transform.position += (Vector3)InputVec;
+        
+        
+	}
+}
